17ef4cb6b63a20e84e7b5ab2c343dd68fccf1f17
Stephanilly <stephanilly@gmail.com>
2024-04-23 22:51:59 +0000

    updated swim choice

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ choice1 = input('You\'re at a cross road. Do you want to go left or right? \n').
 if choice1 == "left":
   choice2 = input('As you wander down the left path for a while, you wonder if there is anything to see. \nEventually, you come to a lake. There\'s an island in the middle of the lake, and you suspect that that might be where the treasure is. \nDo you "wait" for a boat? Or do you "swim" across? \n').lower()
   if choice2 == "wait":
-    choice3 = input("After waiting for what seemed like an eternity, a mysterious boat arrives and you embark. \nYou amazingly arrive at the island unharmed. \nOn the island you approach a house and see 3 doors. One is red, one is yellow and one is blue. \nWhich color door do you choose? \n").lower()
+    choice3 = input("After waiting for what seemed like an eternity, a mysterious boat arrives and you embark. \nYou amazingly arrive at the island unharmed. \nOn the island you approach a house and see 3 doors. One is red, one is yellow, and one is blue. \nWhich color door do you choose? \n").lower()
     if choice3 == "red":
       print("As the door closes behind you, a trap activates and the room is engulfed in flames. You die in agony. Game Over.")
     elif choice3 == "yellow":
@@ -155,7 +155,17 @@ if choice1 == "left":
     else:
       print("You chose a door that doesn't exist and fall forever through the abyss. Game Over.")
   elif choice2 == "swim":
-    print("Since you're a strong swimmer, you decide to try the water; after all, the island doesn\'t look *that* far. \nUnfortunately, the water is infested with angry trout, and you get attacked repeatedly and drown. Game Over.")
+    print('''Since you\'re a strong swimmer, you decide to try the water; after all, the island doesn\'t look *that* far. \nUnfortunately, the water is infested with mutant trout, and you get attacked repeatedly and drown. Game Over.
+            .'|_.-
+         .'  '  /_
+      .-"    -.   '>
+   .- -. -.    '. /    /|_
+  .-.--.-.       ' >  /  /
+ (o( o( o )       \_."  <
+  '-'-''-'            ) <
+(       _.-'-.   ._\.  _\\
+ \'----"/--.__.-) _-  \|
+       "V""    "V"''')
   else:
     print("You've wandered away from the adventure, and the treasure is nowhere to be found. Game Over.")
 elif choice1 == "right":
